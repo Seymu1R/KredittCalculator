fb7b665fcedc76e92845d86174eedb9a4a9845ae
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-07-05 18:59:28 +0400

    final

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-var btn = document.getElementById('btn')
-btn.onclick=()=>{
-    var valueKredit = document.getElementById("kredit").value
-    var valueFaiz=document.getElementById('faiz').value
-    var valueSelect=document.getElementById('select').value
-    var kredit=parseInt(valueKredit);
-    var faiz=parseInt(valueFaiz);
-    var muddet=parseInt(valueSelect);
-    var faizsizMebleg=kredit/muddet;
-    document.getElementById('umumi-mebleg').innerHTML=`${faizsizMebleg}`
-
-
-    
-
-}
+var btn = document.getElementById("btn");
+btn.onclick = () => {
+  var valueKredit = document.getElementById("kredit").value;
+  var valueFaiz = document.getElementById("faiz").value;
+  var valueSelect = document.getElementById("select").value;
+  var kredit = parseInt(valueKredit);
+  var faiz = parseInt(valueFaiz);
+  var muddet = parseInt(valueSelect);
+  var ayliqfaiz = (faiz / 12) * muddet + 100;
+  var umumikridit = (ayliqfaiz * kredit) / 100;
+  var aylıqFaizlə = umumikridit / muddet;
+  var umumiMeblegFaizle = aylıqFaizlə * muddet;
+  document.getElementById("umumi-mebleg").innerHTML = `${umumikridit.toFixed(
+    2
+  )}`;
+  document.getElementById("aylıq-odenis").innerHTML = `${aylıqFaizlə.toFixed(
+    2
+  )}`;
+};
